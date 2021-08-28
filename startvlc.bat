c9bf5d80923b2fc2b4e669826bc18a96799427ec
SpaceSaver <aadixonfamily@gmail.com>
2021-08-28 12:15:18 -0400

    Use videolan instead of purdue for VLC download.

--- startvlc.bat
+++ startvlc.bat
@@ -2,7 +2,7 @@ setlocal
 if exist %temp%\vlcdvdplay del /f /q %temp%\vlcdvdplay
 mkdir %temp%\vlcdvdplay
 set wokabledir=%temp%\vlcdvdplay
-bitsadmin.exe /transfer "DownloadVLC" https://plug-mirror.rcac.purdue.edu/vlc/vlc/3.0.16/win32/vlc-3.0.16-win32.zip %wokabledir%\vlc.zip
+bitsadmin.exe /transfer "DownloadVLC" http://ftp.videolan.org/vlc/3.0.16/win32/vlc-3.0.16-win32.zip %wokabledir%\vlc.zip
 powershell -Command "Expand-Archive -Force '%wokabledir%\vlc.zip' '%wokabledir%\'"
 %wokabledir%\vlc-3.0.16\vlc.exe VIDEO_TS\
 rmdir /s /q %wokabledir%
